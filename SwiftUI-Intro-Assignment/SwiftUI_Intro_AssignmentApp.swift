e8ff623a6f28523b9434babf7388488e4255526a
mrzogy <magbolmrzogy@gmail.com>
2023-05-15 23:51:30 +0300

    Initial Commit

---
+++ SwiftUI-Intro-Assignment/SwiftUI_Intro_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Intro_AssignmentApp.swift
+//  SwiftUI-Intro-Assignment
+//
+//  Created by 3bood on 25/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Intro_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
